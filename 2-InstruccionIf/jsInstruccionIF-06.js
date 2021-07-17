378f05ffab932a8d0a592cb498e17dcff29c4aba
Pamela Freire <pamela.r.freire@gmail.com>
2021-07-17 20:12:53 -0300

    tercer commit

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,20 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
 
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	if (edad >=18){
+		alert("Usted  mayor de edad");
+	} 
+	else if (edad >=13){
+
+		alert("Usted es adolescente");
+
+	}
+	else{
+		alert("Usted es un niño");
+	}
 
 
 }//FIN DE LA FUNCIÓN
